1edf1a43d9b0cbe276c1a184c1f73d902ad7f1e1
Jason Delancey <jason.r.delancey@gmail.com>
2017-10-13 13:11:46 -0400

    Update install_git.sh

--- install_git.sh
+++ install_git.sh
@@ -6,7 +6,7 @@
 
 ## Must run following command to make it executable: chmod a+x install_git.sh
 ## Then run the executable: ./install_git.sh
-## Use valgrind using the syntax: valgrind --tool=memcheck --leak-check=yes executable_name
+## Use valgrind using the syntax: valgrind --tool=memcheck --leak-check=yes ./executable_name
 
 echo "Installing git...."
 sudo apt-get update
